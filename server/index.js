d5d96810ede014388c7dd01df5cc02adf3d4980c
Albert Ho <ah323@cornell.edu>
2020-03-27 01:02:06 -0700

    Implement declare-payment-intent on server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+const stripe = require('stripe')(process.env.STRIPE_SK);
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -11,7 +14,17 @@ app.get('/', (req, res) => {
   res.send({
     backendName: 'stripe-paymentintents',
     status: 'ok',
-  });
+  })
+})
+
+app.post('/declare-payment-intent', async (req, res) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1000,
+    currency: 'usd',
+    metadata: {integration_check: 'accept_a_payment'},
+  })
+
+  res.send({ cs: paymentIntent.client_secret })
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
